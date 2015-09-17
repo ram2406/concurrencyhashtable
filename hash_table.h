a934fb2738a2b19ed7f0e1e3979df1053c225f91
ram2406 <devnull@localhost>
2015-09-18 00:31:41 +0300

    hash_table.h file formatting

--- hash_table.h
+++ hash_table.h
@@ -7,25 +7,25 @@
 
 template <class Key, class Value>
 class HashEntry {
-	
+
 private:
-      Key key;
-      Value value;
+	Key key;
+	Value value;
 public:
-      HashEntry(Key key, Value value)
-		  : key(key), value(value)
-	  {
-      }
- 
+	HashEntry(Key key, Value value)
+		: key(key), value(value)
+	{
+	}
+
 	const Key& getKey() {
 		return key;
-      }
- 
-     Value& getValue() {
-            return value;
-      }
+	}
 
-	 
+	Value& getValue() {
+		return value;
+	}
+
+
 
 	typedef Key first_type;
 	typedef Value second_type;
@@ -100,7 +100,7 @@ public:
 	}
 
 };
- 
+
 template<class Key, class Value>
 class HashMap {
 public:
@@ -109,73 +109,73 @@ public:
 	typedef Key key_type;
 	typedef hash_entry value_type;
 private:
-      std::vector<std::unique_ptr<HashEntry<Key, Value>>> table;
-	  size_t length;
-	  std::atomic_size_t current_size;
-	  hash_type h;
-	  size_t find(const Key& key) const {
-		  size_t hash = (h(key) % (length ));
-            while (table[hash] != nullptr && table[hash]->getKey() != key)
-                  hash = ((hash + 1) % (length ));
-			return hash;
-	  }
-	  static const size_t DefaultCapacity = 1000;
+	std::vector<std::unique_ptr<HashEntry<Key, Value>>> table;
+	size_t length;
+	std::atomic_size_t current_size;
+	hash_type h;
+	size_t find(const Key& key) const {
+		size_t hash = (h(key) % (length ));
+		while (table[hash] != nullptr && table[hash]->getKey() != key)
+			hash = ((hash + 1) % (length ));
+		return hash;
+	}
+	static const size_t DefaultCapacity = 1000;
 public:
-	 HashMap()
+	HashMap()
 		: table(DefaultCapacity), length(DefaultCapacity), current_size(0) {
-            
-      }
-    HashMap(size_t capacity)
+
+	}
+	HashMap(size_t capacity)
 		: table(capacity), length(capacity), current_size(0) {
-            
-      }
-		
-      Value& get(const Key& key)  {
-            const auto& hash = find(key);
-            if (auto& entry = table[hash]) {
-				const auto& before_size = entry->size();
-				auto& value = hash_entry::get(entry, key);
-				current_size += entry->size() - before_size;
-				return value;
-			}
-            else {
-                entry.reset(new hash_entry(key, Value()));
+
+	}
+
+	Value& get(const Key& key)  {
+		const auto& hash = find(key);
+		if (auto& entry = table[hash]) {
+			const auto& before_size = entry->size();
+			auto& value = hash_entry::get(entry, key);
+			current_size += entry->size() - before_size;
+			return value;
+		}
+		else {
+			entry.reset(new hash_entry(key, Value()));
+			++current_size;
+			return entry->getValue();
+		}
+	}
+
+	bool insert(const Key& key, const Value& value) {
+		const auto& hash = find(key);
+		auto& entry = table[hash];
+		if(entry) {
+			const auto& inserted = hash_entry::insert(entry, key, value);
+			if(inserted) {
 				++current_size;
-                return entry->getValue();
 			}
-      }
- 
-      bool insert(const Key& key, const Value& value) {
-           const auto& hash = find(key);
-		   auto& entry = table[hash];
-			   if(entry) {
-				   const auto& inserted = hash_entry::insert(entry, key, value);
-					if(inserted) {
-						++current_size;
-					}
-					return inserted;
-			   }
-			   else {
-				   entry.reset(new hash_entry(key, value));
-				   return true;
-			   }
-		   
-      }     
- 
+			return inserted;
+		}
+		else {
+			entry.reset(new hash_entry(key, value));
+			return true;
+		}
 
-	  Value& operator[] (const Key& key) { return get(key); }
-	  const Value& operator[] (const Key& key) const { return get(key); }
+	}     
 
-	  bool erase(const Key& key) {
-		  const auto& hash = find(key);
-            if (auto& entry = table[hash]) {
-				const auto& removed = hash_entry::erase(entry, key);
-				if( removed ) {
-					--current_size;
-				}
-				return removed;
+
+	Value& operator[] (const Key& key) { return get(key); }
+	const Value& operator[] (const Key& key) const { return get(key); }
+
+	bool erase(const Key& key) {
+		const auto& hash = find(key);
+		if (auto& entry = table[hash]) {
+			const auto& removed = hash_entry::erase(entry, key);
+			if( removed ) {
+				--current_size;
 			}
-			return false;
-	  }
-	  size_t size() const { return current_size; }
+			return removed;
+		}
+		return false;
+	}
+	size_t size() const { return current_size; }
 };
